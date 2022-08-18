b7a2178001bd79adbcc60511d171eb96eefb15ae
Alan Razo <a01703350@itesm.mx>
2022-08-18 12:36:48 -0500

    codigo asincrono

--- Lab8/app.js
+++ Lab8/app.js
@@ -3,3 +3,15 @@ const filesystem = require('fs');
 filesystem.writeFileSync('hola.txt', 'Hola desde node');
 
 console.log("Hola desde node");
+
+const arreglo = [5000, 60, 90, 100, 10, 20, 10000, 120, 20000, 340, 1000, 50];
+
+// of: recorrer el ciclo for 
+// in: imprime el indice del arreglo
+for (let item of arreglo){
+    setTimeout(() => {
+        console.log(item);
+    }, item); 
+}
+
+setTimeout(() => console.log("codigo asincrono"), 7000);
